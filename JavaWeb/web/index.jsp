a7c49849e2eeb23862192cd6ed4351aad8103ebf
gdh050607 <gdh050607@163.com>
2020-03-26 23:03:27 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 86186
+  Date: 2020/3/26
+  Time: 22:06
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>官网</title>
+  </head>
+  <body>
+  <h1 style="color:red"> haha</h1>
+  </body>
+</html>
---
+++ Module01/src/com/gdh/bean/Customer.java
@@ -0,0 +1,31 @@
+package com.gdh.bean;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Customer
+{
+
+
+
+    //验证
+
+    
+    public static void main(String[] args)
+    {
+            System.out.println("module01");
+            ArrayList arrayList;
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("Customer.main");
+
+        String[]  arr;
+
+
+    }
+
+    public void test()
+    {
+
+    }
+}
